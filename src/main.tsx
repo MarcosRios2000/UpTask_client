c62c4291948f25ecafca9d20975cfb943dba857a
MarcosRios2000 <marcosrioos@gmail.com>
2024-08-24 19:44:41 -0300

    feat: create layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Router from './router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1>
-      hola
-    </h1>
+    <Router />
   </StrictMode>,
 )
