9ca79d179a52a818f5ba46250bdec59e05c91162
MarcosRios2000 <marcosrioos@gmail.com>
2024-08-30 22:49:00 -0300

    feat: edit Task flow

--- src/api/TaskAPI.ts
+++ src/api/TaskAPI.ts
@@ -1,10 +1,11 @@
 import { isAxiosError } from "axios";
 import api from "@/lib/axios";
-import { Project, TaskFormData } from "../types";
+import { Project, Task, TaskFormData } from "../types";
 
 type TaskAPI = {
     formData: TaskFormData
     projectId: Project['_id'] 
+    taskId: Task['_id']
 }
 
 export async function createTask({formData, projectId} : Pick<TaskAPI, 'formData' | 'projectId'>) {
@@ -19,3 +20,26 @@ export async function createTask({formData, projectId} : Pick<TaskAPI, 'formData
     }
 }
 
+export async function getTaskById({projectId, taskId} : Pick<TaskAPI, 'projectId' | 'taskId'>) {
+    try {
+        const url = `/projects/${projectId}/tasks/${taskId}`
+        const { data } = await api(url)
+        return data
+    } catch (error) {
+        if(isAxiosError(error) && error.response){
+            throw new Error(error.response.data.error)
+        } 
+    }
+}
+
+export async function updateTask({projectId, taskId, formData}: Pick<TaskAPI, 'formData' | 'projectId' | 'taskId'>) {
+    try {
+        const url = `/projects/${projectId}/tasks/${taskId}`
+        const { data } = await api.put<string>(url, formData)
+        return data
+    } catch (error) {
+        if(isAxiosError(error) && error.response){
+            throw new Error(error.response.data.error)
+        } 
+    }
+}
