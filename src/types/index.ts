ac4615f40109644d1af5e2d40ab05e2939b95dab
MarcosRios2000 <marcosrioos@gmail.com>
2024-12-21 08:57:45 -0300

    feat: change password backend connection

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,6 +5,7 @@ import { z } from "zod";
 export const authSchema = z.object({
     name: z.string(),
     email: z.string().email(),
+    current_password: z.string(),
     password: z.string(),
     password_confirmation: z.string(),
     token: z.string()
@@ -15,7 +16,8 @@ export type UserLoginForm = Pick<Auth, 'email' | 'password'>
 export type UserRegistrationForm = Pick<Auth, 'name' | 'email' | 'password' | 'password_confirmation'>
 export type RequestConfirmationCodeForm = Pick<Auth, 'email'>
 export type ForgotPasswordForm = Pick<Auth, 'email'>
-export type NewPasswordForm = Pick<Auth, 'password' | 'password_confirmation' >
+export type NewPasswordForm = Pick<Auth, 'password' | 'password_confirmation'>
+export type UpdateCurrentUserPasswordForm = Pick<Auth, 'current_password' | 'password' | 'password_confirmation'>
 
 /** Users */
 export const userSchema = authSchema.pick({
