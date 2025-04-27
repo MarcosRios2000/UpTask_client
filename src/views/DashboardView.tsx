8eb91ae4db83eace2f4288b45e516094b4a8c506
MarcosRios2000 <marcosrioos@gmail.com>
2025-04-27 15:10:54 -0300

    feat: add custom SVG favicon and configure correct MIME type

--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -23,7 +23,7 @@ export default function DashboardView() {
       <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
         <div className="flex flex-col items-center gap-4">
           <div className="w-12 h-12 border-4 border-fuchsia-500 border-t-transparent rounded-full animate-spin"></div>
-          <p className="text-gray-600 text-lg font-semibold">Loasdasading...</p>
+          <p className="text-gray-600 text-lg font-semibold">Loading...</p>
         </div>
       </div>
     )
