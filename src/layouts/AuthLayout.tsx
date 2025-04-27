129fa1786fc2e9f3c8abab7f8eb7240f0b9f7e55
MarcosRios2000 <marcosrioos@gmail.com>
2025-04-27 16:15:19 -0300

    fix: responsive styles in auth layout

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -3,24 +3,24 @@ import { Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
 
-
 export default function AuthLayout() {
-  return (
-    <>
-        <div className='bg-gray-800 min-h-screen'>
-            <div className='py-10 lg:py-20 mx-auto w-[450px]'>
-                <Logo />
-
-                <div className='mt-10'>
-                    <Outlet />
-                </div>
+    return (
+      <>
+        <div className="min-h-screen bg-gray-800 flex items-center justify-center overflow-x-hidden px-4">
+          <div className="w-full max-w-md py-10 lg:py-20">
+            <Logo />
+  
+            <div className="mt-10">
+              <Outlet />
             </div>
+          </div>
         </div>
-
+  
         <ToastContainer
-            pauseOnHover={false}
-            pauseOnFocusLoss={false}
+          pauseOnHover={false}
+          pauseOnFocusLoss={false}
         />
-    </>
-  )
-}
+      </>
+    )
+  }
+  
