b00852238b31ebcbb01e79ef5fd7e01adba23c6b
MarcosRios2000 <marcosrioos@gmail.com>
2025-04-27 16:31:32 -0300

    fix: responsive status drag n drop disabled in mobile

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -7,6 +7,8 @@ import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { updateStatus } from '@/api/TaskAPI'
 import { toast } from 'react-toastify'
 import { useParams } from 'react-router-dom'
+import { isMobile } from '@/utils/isMobile'
+
 
 type TaskListProps = {
     tasks: TaskProject[]
@@ -24,7 +26,6 @@ const initialStatusGroups : GroupedTasks = {
     underReview: [],
     completed: []
 }
-
 
 const statusStyles : { [ key: string ] : string } = {
     pending: 'border-t-slate-500',
@@ -84,32 +85,57 @@ export default function TaskList({tasks, canEdit} : TaskListProps) {
         } 
     }
 
+    const dragAndDropEnabled = !isMobile()
+
   return (
     <>
-           <h2 className="text-5xl font-black my-10">Tasks</h2>
-            <div className='flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32'>
-                <DndContext onDragEnd={handleDragEnd}>
-                {Object.entries(groupedTasks).map(([status, tasks]) => (
-                    <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
-                        <h3 
-                        className={`capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8 ${statusStyles[status]}`}
-                        >
-                            {statusTranslations[status]}
-                        </h3>
+      <h2 className="text-5xl font-black my-10">Tasks</h2>
+      <div className='flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32'>
+        {dragAndDropEnabled ? (
+          <DndContext onDragEnd={handleDragEnd}>
+            {Object.entries(groupedTasks).map(([status, tasks]) => (
+              <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
+                <h3 
+                  className={`capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8 ${statusStyles[status]}`}
+                >
+                  {statusTranslations[status]}
+                </h3>
 
-                        <DropTask status={status}/>
+                <DropTask status={status}/>
 
-                        <ul className='mt-5 space-y-5'>
-                            {tasks.length === 0 ? (
-                                <li className="text-gray-500 text-center pt-3">No tasks yet</li>
-                            ) : (
-                                tasks.map(task => <TaskCard key={task._id} task={task} canEdit={canEdit} />)
-                            )}
-                        </ul>
-                    </div>
-                ))}
-                </DndContext>
-            </div>
+                <ul className='mt-5 space-y-5'>
+                  {tasks.length === 0 ? (
+                    <li className="text-gray-500 text-center pt-3">No tasks yet</li>
+                  ) : (
+                    tasks.map(task => <TaskCard key={task._id} task={task} canEdit={canEdit} />)
+                  )}
+                </ul>
+              </div>
+            ))}
+          </DndContext>
+        ) : (
+          // Mobile view sin DnD
+          <>
+            {Object.entries(groupedTasks).map(([status, tasks]) => (
+              <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
+                <h3 
+                  className={`capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8 ${statusStyles[status]}`}
+                >
+                  {statusTranslations[status]}
+                </h3>
+
+                <ul className='mt-5 space-y-5'>
+                  {tasks.length === 0 ? (
+                    <li className="text-gray-500 text-center pt-3">No tasks yet</li>
+                  ) : (
+                    tasks.map(task => <TaskCard key={task._id} task={task} canEdit={canEdit} />)
+                  )}
+                </ul>
+              </div>
+            ))}
+          </>
+        )}
+      </div>
     </>
   )
 }
